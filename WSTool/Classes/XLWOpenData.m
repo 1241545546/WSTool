ed03661c236dced8cde745e1f508fbf3f2bd0920
1241545546@qq.com <1241545546@qq.com>
2018-11-23 11:09:44 +0800

    init

---
+++ WSTool/Classes/XLWOpenData.m
@@ -0,0 +1,17 @@
+//
+//  XLWOpenData.m
+//  WSTool_Example
+//
+//  Created by sawu on 2018/11/23.
+//  Copyright © 2018年 wusaYa. All rights reserved.
+//
+
+#import "XLWOpenData.h"
+
+@implementation XLWOpenData
+
++(void)openData{
+    NSLog(@"you are shabi ");
+}
+
+@end
