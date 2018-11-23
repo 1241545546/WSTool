e802760a6b123ad9de30336c0247e23e551af644
sawu <sawu@sawudeiMac.local>
2018-11-23 10:49:28 +0800

    init project

---
+++ WSTool/Classes/WSStoll.h
@@ -0,0 +1,12 @@
+//
+//  WSStoll.h
+//  FBSnapshotTestCase
+//
+//  Created by sawu on 2018/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WSStoll : NSObject
+
+@end
---
+++ WSTool/Classes/WSStoll.m
@@ -0,0 +1,12 @@
+//
+//  WSStoll.m
+//  FBSnapshotTestCase
+//
+//  Created by sawu on 2018/11/22.
+//
+
+#import "WSStoll.h"
+
+@implementation WSStoll
+
+@end
